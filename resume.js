7ac73975bc76608bb3be479c780b303414c4c0d6
Caleb Pendergraft <cackmed@gmail.com>
2020-02-03 16:57:12 -0800

    skeleton created

---
+++ resume.js
@@ -0,0 +1,35 @@
+
+const data = {
+  'header' : {
+    'name' : 'Caleb Pendergraft',
+    'jobTitle' : 'Full-Stack Developer',
+    'email' : 'cackmed@gmail.com',
+    'github' : 'https://github.com/cackmed',
+    'linkedin' : 'https://www.linkedin.com/in/calebpendergraft/'
+  },
+  'AboutMe' : 'I am a Full-Stack JavaScript Developer with a background with entrepreneurial endeavours in the ecommerce sphere, and a passion for learning new skills. Looking to work in a collaborative fast paced environment where I can continue to learn.',
+  'tech' : {
+    'languages' : ['HTML', 'CSS', 'JavaScript'],
+    'back-end' : ['Node.js', 'Express'],
+    'front-end' : ['React', 'Redux'],
+    'databases' : ['MongoDB', 'PostgreSQL'],
+    'tools' : ['Git', 'Github', 'VScode', 'Postman', 'Compass', 'Slack'],
+    'dev-practices' : ['TDD', 'BDD', 'aglie'],
+    'test' : ['JEST', 'QUnit', 'TravisCL']
+  },
+  'anteUp' : {
+    'name' : 'Ante-UP',
+    'type' : 'Back-End',
+    'tech' : 'avaScript, Node, Express, MongoDB , Socket.io, Inquirer',
+    'desc' : 'Terminal based multiplayer game of texas holdem.'
+  },
+  'rogueLike' : {
+    'name' : 'Columns And Rogues',
+    'type' : 'Full-Stack',
+    'tech' : 'HTML, JavaScript, Node, Express, PostgresSQL, CSS',
+    'desc' : 'Online persistent roguelike that we built from the ground up'
+  }
+
+};
+
+export default data;
--- src/components/Tech.js
+++ src/components/Tech.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Proptypes from 'prop-types';
 
-const Tech = ({ languages, frontEnd, backEnd, tools, other }) => (
+const Tech = ({ languages, frontEnd, backEnd, databases, tools, other }) => (
   <>
     <p>{languages}</p>
     <p>{frontEnd}</p>
     <p>{backEnd}</p>
+    <p>{databases}</p>
     <p>{tools}</p>
     <p>{other}</p>
   </>
@@ -15,6 +16,7 @@ Tech.proptypes = {
   languages: Proptypes.string.isRequired,
   frontEnd: Proptypes.string.isRequired,
   backEnd: Proptypes.string.isRequired,
+  databases: Proptypes.string.isRequired,
   tools: Proptypes.string.isRequired,
   other: Proptypes.string.isRequired,
 };
